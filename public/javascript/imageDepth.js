d3fba193931647adc4b8458df91fc019f2883300
Cadenza <mirandalucsb@gmail.com>
2022-03-05 14:05:35 -0700

    tweaked image position

--- public/javascript/imageDepth.js
+++ public/javascript/imageDepth.js
@@ -1,6 +1,5 @@
 
-let app = new PIXI.Application({width: 930, height: 675});
-console.log(app);
+let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
 document.body.appendChild(app.view);
 
 let img = new PIXI.Sprite.from("/images/storage_unit_hallway_1.png");
@@ -12,11 +11,14 @@ let candleImg = new PIXI.Sprite.from("images/small candle.png");
 
 candleImg.width = 700;
 candleImg.height = 700;
+candleImg.x = 100;
+candleImg.y = 0;
 app.stage.addChild(candleImg);
 
 let depthMap = new PIXI.Sprite.from("/images/small candle_depth.png");
 depthMap.width = 700;
 depthMap.height = 700;
+depthMap.x = 100;
 app.stage.addChild(depthMap);
 
 let colorMatrix = new PIXI.filters.ColorMatrixFilter();
@@ -36,6 +38,6 @@ candleImg.filters = [displacementFilter];
 
 //added mouse movement to emphasize 3D effect
 window.onmousemove = (e) => {
-    displacementFilter.scale.x = (700/2 - e.clientX) / 20;
-    displacementFilter.scale.y = (700/2 - e.clientY) / 20;
+    displacementFilter.scale.x = (700/2 - Math.min(e.clientX,700)) / 20;
+    displacementFilter.scale.y = (700/2 - Math.min(e.clientY,700)) / 20;
 } 
